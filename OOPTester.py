7c538d2dacc5410bfc7d7b911bcc4a75d0f76d6d
FatimaMoqran <fatima.moqran@gmail.com>
2019-07-12 12:39:57 +0000

    preparation des paramètres avant codage backward class

--- OOPTester.py
+++ OOPTester.py
@@ -1,6 +1,28 @@
 from  create_dataset import Create_Xor
+from activation import Activation
+from forward import Forward
 
 myDataset = Create_Xor()
 
-myDataset.print_shape()
-myDataset.print_XY()
+#myDataset.print_shape()
+#myDataset.print_XY()
+
+X =  myDataset.get_X()
+annForward = Forward([2,3,1])
+
+#print(annForward.parameters)
+parameters = annForward.parameters
+# print(parameters)
+annForward.forward_layers(X)
+
+
+# print(annForward.AL)
+# print(annForward.caches)
+Y = myDataset.get_Y()
+
+#get the forward and caches 
+cost=annForward.compute_cost(Y)
+
+caches = annForward.caches
+print(cost)
+print(caches)
