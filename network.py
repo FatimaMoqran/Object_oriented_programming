de54ff6601d90d72941fc0644baceb056527ade6
FatimaMoqran <fatima.moqran@gmail.com>
2019-07-15 10:09:16 +0000

    creation d'une classe Network et implementation de fonctions

---
+++ network.py
@@ -0,0 +1,279 @@
+
+import numpy as np 
+
+
+class Network(Activation): 
+    """
+        class that make forward and Backward
+    """
+    #propriétés
+    
+    
+    def __init__(self,dim_layers):
+
+        """ 
+          Function that initialize weights and biais for each layer
+          :param dim_layers: list of each layer
+          :type dim_layers: pyhon list
+          :return parameters dictionnary with W1,b1,......WL,bL
+          :rtype: python dictionnary
+          Wl --- weight matrix of shape (dim_layers[l],dim_layers[1-l])
+          b1 --- weight matrix of shape (dim_layer[l],dim_layers[1-l])
+        """
+        self.caches = []
+        self.AL  = None
+        self.dim_layers = dim_layers
+        self.cost = None
+        np.random.seed(3)
+        self.parameters={}
+
+        self.L = len(self.dim_layers)
+
+        for l in range(1,self.L):
+
+            self.parameters[f'W{l}']= np.random.randn(self.dim_layers[l], self.dim_layers[l-1])*0.01
+
+            self.parameters[f'b{l}'] = np.zeros((self.dim_layers[l],1))
+
+            print(f'W{l}.shape = {self.dim_layers[l]},{self.dim_layers[l-1]}')
+            print(f"b{l}.shape = {self.dim_layers[l]}, 1 ")
+
+            assert (self.parameters[f"W{l}"].shape == (self.dim_layers[l],self.dim_layers[l-1]))
+            assert (self.parameters[f"b{l}"].shape == (self.dim_layers[l],1))
+
+        return None
+
+
+    def linear_activation_forward (self,A_prev ,W , b, activation):
+        """
+          function that both computes preactivation and activation 
+          :param A_prev: previews activation matrix (for the first layer it is X )
+          :param W: weight matrix for the current layer
+          :param b: biais matrix for the current layer
+          :param activation: choice of activation function eg: sigmoid , relu
+          :type A_prev : matrix of float
+          :type W: matrix of float
+          :type b: matrix of float
+          :return: A matrix of activation
+          :cache: tuple of (linear_cache,activation_cache) 
+          """
+
+        Z = np.dot(W,A_prev)+b
+
+        linear_cache = (A_prev,W,b)
+
+
+        assert(Z.shape == (W.shape[0], A_prev.shape[1]))
+
+        if activation == "sigmoid":
+
+            A = self.sigmoid(Z)
+            activation_cache = A
+
+        elif activation == "relu":
+
+            A = self.relu(Z)
+            activation_cache = A
+
+
+        assert (A.shape == Z.shape)
+
+        cache = (linear_cache, activation_cache)
+
+        return A,cache
+
+    def forward_layers(self,X):
+        """
+           Function that computes the forward activation for L layers
+          :param X: input matrix, shape (input_size, number of exemple)
+          :param parameters: output of initialisation_deep dictionnary of W, b
+          :type X: matrix of float
+          :type parameters : dictionary of matrices
+          :return AL : last post activation value
+          :return caches : list of caches with every caches of linear_activation_forward 
+          :rtype AL: matrix of float
+          :rtype caches: list of tuples
+
+      """
+        #je crée une  liste de  caches où je vais stoker les valeurs obtenues
+        
+        A = X
+        L = len(self.parameters) // 2 # je calcul le nombre de couches par rapport aux nombres de paramètres
+        #je fais une boucle pour toutes les couches jusqu'à L-1
+        for l in range (1,L): 
+        #je considère X comme 
+            A_prev= A
+    #       je fais mon calcul pour A0 jusqu'à L-1 ou bien (1 à L)
+            A,cache = self.linear_activation_forward(A_prev,self.parameters[f'W{l}'],self.parameters[f'b{l}'],"sigmoid")
+
+            #je rajoute le cache obtenu dans la liste cache
+            self.caches.append(cache)
+
+          #calcul pour la dernière couche:
+          #je récupère le dernier A qui est sorti de mes couches précédentes et je lui mets une sigmoid 
+
+        self.AL,cache = self.linear_activation_forward(A,self.parameters[f'W{l+1}'],self.parameters[f'b{l+1}'],"sigmoid")
+        self.caches.append(cache)
+
+        assert self.AL.shape == (1,X.shape[1])
+
+        return None
+
+       
+    def compute_cost(self,Y):
+        """
+            Function that compute the cost 
+            :param AL: probability vector - shape (1,number of examples)
+            :param Y:  matrix of float
+            :type AL: matrix
+            :type Y: array of booleen
+            :return cost: cost result
+            :rtype: float 
+        """
+        m = Y.shape[1]
+
+        #je calcule d'abord llog
+        logprob = (Y * np.log(self.AL) + (1-Y) * np.log(1-self.AL))
+        #ensuite la cost
+        self.cost = -(np.sum(logprob))/m
+        #je veux que l'on me retourne un nombre et non pas un array
+        self.cost = np.squeeze(self.cost)
+        #être sur que j'ai la cost au bon format
+        assert(isinstance(self.cost,float))
+
+        return self.cost
+
+
+    def linear_backward(self,dZ,cache):
+        """
+            function that computes the linear backward
+            :param dZ: gradient of the cost with respect to linear output
+            :param cache: tuple of value
+            :return dA_prev: gradient of the cost with respect to activation
+            :return dW: gradient of the cost with respect to W
+            :return db: gradient of the cost with respect to b
+        """
+        #recuperation des valeurs dont j'ai besoin
+
+        A_prev, W, b = cache
+        m=A_prev.shape[1]
+        #calcul des dérivées:
+
+        dW = np.dot(dZ,A_prev.T)/m
+        db = np.sum(dZ,axis=1,keepdims = True)/m
+        dA_prev= np.dot(W.T,dZ)
+
+        assert(dA_prev.shape == A_prev.shape)
+        assert(dW.shape == W.shape)
+        assert(db.shape == b.shape)
+
+        return dA_prev,dW,db    
+    
+    def linear_activation_backward(self,dA,cache,activation):
+        """  
+            function that computes the linear activation backward with sigmoid and relu
+            :param dA: gradient of the activation for the current layer the l layer
+            :param cache: tuple of values with the parameters
+            :param activation: activation function-Relu or Sigmoid
+            :type dA: numpy array
+            :type activation: string
+            :return dA_prev: gradient activation of the l-1 layer-shape = A_prev shape
+            :return dW: gradient of the cost with respect of the W for the current layer l
+            :return db: gradient of the cost with respect of the b for the current layer l
+        """
+        #je récupère de mon cache les paramètres 
+
+        linear_cache, activation_cache = cache
+
+        # je calcule dZ en fonction de l'activation RELU ou Sigmoid
+        if activation == 'relu':
+            dZ = self.derivative_relu(dA, activation_cache)
+            dA_prev,dW,db = self.linear_backward (dZ,linear_cache)
+
+        elif activation == 'sigmoid':
+            dZ = self.derivative_sigmoid(dA, activation_cache)
+            dA_prev, dW, db = self.linear_backward(dZ,linear_cache)
+
+        return dA_prev, dW, db
+
+     def l_model_backward(self):
+
+            """ 
+                  function that compute the backward propagation
+                  :param AL: array with the last activation -probability vector 
+                  :param Y: array with the label
+                  :param caches: list of caches of all the parameters of relu activation and one cache with all the parameters with sigmoid
+                  :type AL: numpy array
+                  :type Y: vectord
+                  :type caches: python list
+                  :return grads: dictionnary of gradients dW,db,dA
+
+            """
+
+        #     print("initialisation de grad")
+            L = len(self.caches) #nombre de couches (correspond aux nombres de caches)
+            m = self.AL.shape[1]
+            self.Y = self.Y.reshape(self.AL.shape) #on reshape Y comme AL pour pouvoir faire les opérations
+        #     print("L = ", L, " m = ",m, " Y = ",Y)
+            #     initialisation de la back propagation pour calculer dAL
+
+
+            dAL = -(np.divide(self.Y,self.AL)-np.divide(1-self.Y,1-self.AL))
+        #     print("dAL=", dAL)
+
+
+              #calcul des gradients pour la dernière couche L sigmoid 
+            #j'utilise le cache de la dernière couche et je mets tout dans un dictionnaire
+
+            current_cache = self.caches[L-1]
+        #     print("current_cache = ", current_cache)
+            self.grads[f"dA{L-1}"], self.grads[f"dW{L}"], self.grads[f"db{L}"] = self.linear_activation_backward(dAL,current_cache, activation ="sigmoid")
+        #     print([f"dA{L-1}"], grads[f"dW{L}"], grads[f"db{L}"])
+
+            #ensuite je fais une boucle pour les autres couches de l= l-2 à l = 0
+
+            for l in reversed(range(L-1)):
+                """ 
+        #             entrée : la dérivée dA l+1 et le cache de la couche current
+        #             sortie : la dérivée dA l et dWl+1 et dbl+1
+
+        #         """
+
+                current_cache = self.caches[l]
+
+        #         print("l = ", l , "current_cache W =", current_cache[0][1].shape)
+        #         #je crée des variables temporaires: dA_prev_temp, dW_temp, db_temp
+
+                dA_prev_temp, dW_temp, db_temp = self.linear_activation_backward (self.grads[f"dA{l+1}"], current_cache, activation = "sigmoid")
+
+        # #         #je les mets dans le dictionnaire
+
+                self.grads[f"dA{l}"] = dA_prev_temp
+                self.grads[f"dW{l+1}"] = dW_temp
+                self.grads[f"db{l+1}"] = db_temp
+
+                print ("dA", l, self.grads[f"dA{l}"].shape)
+                print ("dW", l+1, self.grads[f"dW{l+1}"].shape)
+                print ("db", l+1, self.grads[f"db{l+1}"].shape)
+
+
+            return self.grads
+        
+
+    def update_parameters(self,learning_rate = 0.5):
+            """
+                function that update parameters using the gradient descent
+                :argument parameters: python dictionary with parameters
+                :argument grads: python dictionnary with all the gradient
+                :return parameters: python dictionnar with the updated parameters
+            """
+            L = len (self.parameters)//2
+            for l in range(L):
+                self.parameters["W" + str(l+1)] = self.parameters["W" + str(l+1)] - learning_rate * self.grads["dW" + str(l+1)]
+
+                self.parameters["b" + str(l+1)] = self.parameters["b" + str(l+1)] - learning_rate * self.grads["db" + str(l + 1)]
+
+            return self.parameters
+
+
+   
